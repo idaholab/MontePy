84f7ff3a7259c5cfd61af371473948590e2d08dc
Micah Gale <micah.gale@inl.gov>
2021-11-05 14:13:56 -0600

    Created data card constructor and static method

--- mcnpy/data_card.py
+++ mcnpy/data_card.py
@@ -1,3 +1,4 @@
+from .material import Material
 from .mcnp_card import MCNP_Card
 
 
@@ -7,13 +8,43 @@ class DataCard(MCNP_Card):
     """
 
     def __init__(self, input_card, comment=None):
-        """"""
-        pass
+        """
+        :param input_card: the Card object representing this data card
+        :type input_card: Card
+        :param comment: The Comment that may proceed this
+        :type comment: Comment
+        """
+        super().__init__(comment)
+        self.__words = input_card.words
+
+    @property
+    def words(self):
+        """
+        The words of the data card, not parsed.
+
+        :rtype: list
+        """
+        return self.__words
 
     def format_for_mcnp_input(self):
         pass
 
     @staticmethod
-    def parse_data(cls, input_card, comment=None):
-        """"""
-        pass
+    def parse_data(input_card, comment=None):
+        """
+        Parses the data card as the appropriate object if it is supported.
+
+        :param input_card: the Card object for this Data card
+        :type input_card: Card
+        :param comment: the Comment that may proceed this.
+        :type comment: Comment
+        :return: the parsed DataCard object
+        :rtype: DataCard
+        """
+        identifier = input_card.words[0]
+
+        #material finder
+        if "m" in identifier.lower():
+            return Material(input_card, comment)
+        else:
+            return DataCard(input_card, comment)
