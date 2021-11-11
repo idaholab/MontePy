5672047d65af9c604efb4cae880cfb1647030333
Micah Gale <micah.gale@inl.gov>
2021-11-11 13:04:17 -0700

    Exposed lower level object

--- mcnpy/__init__.py
+++ mcnpy/__init__.py
@@ -2,7 +2,7 @@ name = "MCNPy"
 __version__ = "0.0.2"
 
 from .mcnp_problem import MCNP_Problem
-from .input_parser import read_input
+from .input_parser import read_input, Card, Comment, Message, Title, BlockType
 from .surfaces import Surface
 from .cell import Cell
 from  .errors import *
