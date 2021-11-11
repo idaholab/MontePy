14788c1d8e119c327302078303c21cc0e7a9eb9f
Micah Gale <micah.gale@inl.gov>
2021-11-11 09:19:34 -0700

    Added explicit surface type objects

---
+++ mcnpy/surfaces/cylinder_on_axis.py
@@ -0,0 +1,8 @@
+from .surface_type import SurfaceType
+from .surface import Surface
+
+class CylinderOnAxis(Surface):
+    """
+    Represents surfaces: CX, CY, CZ
+    """
+    pass
---
+++ mcnpy/surfaces/cylinder_par_axis.py
@@ -0,0 +1,63 @@
+from .. import surface_type
+from .. import surface
+
+class CylinderParAxis(surface.Surface):
+    """
+    Represents surfaces: C/X, C/Y, C/Z
+    """
+    def __init__(self, input_card, comment=None):
+
+        COORDINATE_PAIRS = {
+            surface_type.SurfaceType.C_X: {0: "y", 1:"z"},
+
+
+        }
+        """
+        :param input_card: The Card object representing the input
+        :type input_card: Card
+        :param comment: the Comment object representing the
+                        preceding comment block.
+        :type comment: Comment
+        """
+        super().__init__(input_card, comment)
+        ST = surface_type.SurfaceType
+        assert self.surface_type in [ST.C_X, ST.C_Y, ST,C_Z]
+        assert len(self.constants) == 3
+        self.__coordinates = self.constants[0:2]
+        self.__radius = self.constant[2]
+
+
+    @property
+    def coordinates(self):
+        """
+        The two coordinates for this cylinder to center on.
+        
+        :rytpe: list
+        """
+        return self.__coordinates
+
+    @coordinates.setter
+    def coordinates(self, coordinates):
+        """
+        :param coordinates: the coordinates, must be 2 long.
+        """
+        assert len(coordinates) == 2
+        self.__coordinates = coordinates
+        self.__constants[0:2] = coordinates
+
+    @property
+    def radius(self):
+        """
+        The radius of the cylinder.
+
+        :rtype: float
+        """
+        return self.__radius
+        
+    @radius.setter
+    def radius(self, radius):
+        assert isinstance(radius, float)
+        assert radius > 0
+        self.__radius = radius
+        self.__constants[2] = radius
+
