5ea3b34b60feca37c42ffb16af6b0cc73ec1107e
Micah Gale <micah.gale@inl.gov>
2022-01-28 09:07:06 -0700

    Fixed issues with read card.
    -1 made sure that the global queue gets cleared
    -2 made opening the file relative to the parent file
    -3 ensured StringIO files do not activate read files (avoid test case
    triggering)

--- mcnpy/input_parser/input_syntax_reader.py
+++ mcnpy/input_parser/input_syntax_reader.py
@@ -1,8 +1,10 @@
 from .block_type import BlockType
 from .. import errors
 import itertools
+import io
 from mcnpy.input_parser.mcnp_input import Card, Comment, Message, ReadCard, Title
 import re
+import os
 
 BLANK_SPACE_CONTINUE = 5
 reading_queue = []
@@ -19,6 +21,8 @@ def read_input_syntax(input_file):
     :param input_file: the path to the input file to be read
     :type input_file: str
     """
+    global reading_queue
+    reading_queue = []
     with open(input_file, "r") as fh:
         yield from read_front_matters(fh)
         yield from read_data(fh)
@@ -138,10 +142,13 @@ def read_data(fh, block_type=None, recursion=False):
         yield generate_card_object(block_type, words)
 
     if not recursion:
-        for block_type, file_name in reading_queue:
-            with open(file_name, "r") as sub_fh:
-                for input_card in read_data(sub_fh, block_type, True):
-                    yield input_card
+        # ensure fh is a file reader, ignore StringIO
+        if isinstance(fh, io.TextIOWrapper):
+            path = os.path.dirname(fh.name)
+            for block_type, file_name in reading_queue:
+                with open(os.path.join(path, file_name), "r") as sub_fh:
+                    for input_card in read_data(sub_fh, block_type, True):
+                        yield input_card
 
 
 def generate_card_object(block_type, words):
