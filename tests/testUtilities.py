a0ab837ae6b33c53c25104274d2dda56453c7007
Micah Gale <micah.gale@inl.gov>
2022-01-28 09:27:49 -0700

    Added tests for the fortran float parser

---
+++ tests/testUtilities.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from mcnpy.utilities import fortran_float
+
+
+class testFortranFloat(TestCase):
+    def test_normal_float_parse(self):
+        tests = {"123": 123, "1.23": 1.23, "1.2e+3": 1.2e3, "1.2e-3": 1.2e-3}
+        for test_string in tests:
+            self.assertAlmostEqual(fortran_float(test_string), tests[test_string])
+
+    def test_stupid_float_parse(self):
+        tests = {"1.2+3": 1.2e3, "1.2-3": 1.2e-3}
+        for test_string in tests:
+            self.assertAlmostEqual(fortran_float(test_string), tests[test_string])
+
+    def test_raise_error(self):
+        with self.assertRaises(ValueError):
+            fortran_float("Dog")
