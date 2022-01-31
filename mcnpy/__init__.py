cbccbaaf520d94e532285059c759fc04e36830e0
Micah Gale <micah.gale@inl.gov>
2022-01-31 16:54:27 -0700

    Made top level init imports

--- mcnpy/__init__.py
+++ mcnpy/__init__.py
@@ -4,3 +4,5 @@ __all__ = ["cell", "surface", "mcnp_card", "input_parser"]
 
 from . import input_parser
 from .input_parser.input_reader import read_input
+from mcnpy.cell import Cell
+from mcnpy.data_cards.material import Material
