ffc257658d9740cc1b92e95347a39b21c7d8d2b3
Micah Gale <micah.gale@inl.gov>
2022-02-15 13:09:27 -0700

    Removed duplicate finder for general plane; too immature to support

--- mcnpy/surfaces/general_plane.py
+++ mcnpy/surfaces/general_plane.py
@@ -13,34 +13,4 @@ class GeneralPlane(Surface):
         assert len(self.surface_constants) in {4, 9}
 
     def find_duplicate_surfaces(self, surfaces, tolerance):
-        ret = []
-        if not self.old_periodic_surface:
-            for surface in surfaces:
-                if surface != self and surface.surface_type == self.surface_type:
-                    if not self.old_periodic_surface:
-                        match = True
-                        for i, constant in enumerate(self.surface_constants):
-                            if (
-                                abs(constant - surface.surface_constants[i])
-                                >= tolerance
-                            ):
-                                match = False
-                        # Try the anti-vector
-                        if not match:
-                            match = True
-                            for i, constant in enumerate(self.surface_constants):
-                                if (
-                                    abs(-constant - surface.surface_constants[i])
-                                    >= tolerance
-                                ):
-                                    match = False
-                        if match and self.transform:
-                            match = False
-                            if surface.transform:
-                                if self.transform.equivalent(
-                                    surface.transform, tolerance
-                                ):
-                                    match = True
-                        if match:
-                            ret.append(surface)
-        return ret
+        return []
