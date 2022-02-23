c81d984ce5c0ec156e091faec74f85834c812578
Micah Gale <micah.gale@inl.gov>
2022-02-22 17:11:06 -0700

    Created surfaces collection

---
+++ mcnpy/surface_collection.py
@@ -0,0 +1,23 @@
+from mcnpy.collections import Collection
+from mcnpy.surfaces.surface import Surface
+
+
+class Surfaces(Collection):
+    def __init__(self, surfaces=None):
+        super().__init__(Surface, surfaces)
+
+    @property
+    def numbers(self):
+        for surf in self._objects:
+            yield surf.surface_number
+
+    def append(self, surface):
+        assert isinstance(surface, Surface)
+        self._objects.append(surface)
+
+    def __iadd__(self, other):
+        assert type(other) in [Surfaces, list]
+        for surface in other:
+            assert isinstance(surface, Surface)
+        self._objects += other._objects
+        return self
