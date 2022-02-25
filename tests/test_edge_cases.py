0bdc905e50d6d9a42b273f50c5d34bfc3c94f0c2
Micah Gale <micah.gale@inl.gov>
2022-02-25 16:34:01 -0700

    Found edge case for the interpolate in whole core model

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -8,3 +8,7 @@ class EdgeCaseTests(TestCase):
         capsule = mcnpy.read_input("tests/inputs/test_complement_edge.imcnp")
         complementing_cell = capsule.cells[2]
         self.assertEqual(len(complementing_cell.complements), 2)
+
+    def test_interp_surface_edge_case(self):
+        capsule = mcnpy.read_input("tests/inputs/test_interp_edge.imcnp")
+        self.assertEqual(len(capsule.cells[0].surfaces), 4)
