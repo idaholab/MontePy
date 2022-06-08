7dd208137bfbea12f43836308b64f15dee22c46e
Micah Gale <micah.gale@inl.gov>
2022-06-08 13:34:38 -0600

    Added edge case for SC

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -51,3 +51,8 @@ class EdgeCaseTests(TestCase):
             [in_str], mcnpy.input_parser.block_type.BlockType.DATA
         )
         self.assertEqual(card.words, in_str.split())
+        in_str = in_str.replace("fc247", "sc247")
+        card = mcnpy.input_parser.mcnp_input.Card(
+            [in_str], mcnpy.input_parser.block_type.BlockType.DATA
+        )
+        self.assertEqual(card.words, in_str.split())
