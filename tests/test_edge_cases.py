7097399146d89232386305192d0fd5e581bd4ef4
Micah Gale <micah.gale@inl.gov>
2022-06-14 08:25:08 -0600

    Tested failing tests reports

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -6,6 +6,9 @@ from unittest import TestCase
 
 
 class EdgeCaseTests(TestCase):
+    def test_fail(self):
+        self.assertTrue(False)
+
     def test_complement_edge_case(self):
         capsule = mcnpy.read_input("tests/inputs/test_complement_edge.imcnp")
         complementing_cell = capsule.cells[61482]
