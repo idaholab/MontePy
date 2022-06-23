ebb19b830684464ce5bfbd59b9a08f372cce80a3
Micah Gale <micah.gale@inl.gov>
2022-06-23 13:02:09 -0600

    Reformatted with black

--- mcnpy/input_parser/constants.py
+++ mcnpy/input_parser/constants.py
@@ -9,21 +9,18 @@ Prior MCNP release version numbers were taken from RSICC:
     https://rsicc.ornl.gov/codes/ccc/ccc8/ccc-810.html
 """
 
-LINE_LENGTH = {
-    (5, 1, 60): 80,
-    (6, 1, 0): 80,
-    (6, 2, 0): 128
-}
+LINE_LENGTH = {(5, 1, 60): 80, (6, 1, 0): 80, (6, 2, 0): 128}
 
-DEFAULT_VERSION = (6,2,0)
+DEFAULT_VERSION = (6, 2, 0)
 
-def get_max_line_length(mcnp_version= DEFAULT_VERSION):
+
+def get_max_line_length(mcnp_version=DEFAULT_VERSION):
     """
     Gets the maximum allowed length for an input line for a specific MCNP version.
 
-    The version must be a three component tuple e.g., (6, 2, 0) and (5, 1, 60). 
+    The version must be a three component tuple e.g., (6, 2, 0) and (5, 1, 60).
 
-    :param mcnp_version: The version of MCNP that the input is intended for. 
+    :param mcnp_version: The version of MCNP that the input is intended for.
     :type mcnp_version: tuple
     :returns: The number of characters allowed in a line.
     :rtype: int
@@ -34,4 +31,3 @@ def get_max_line_length(mcnp_version= DEFAULT_VERSION):
         return LINE_LENGTH[mcnp_version]
     except KeyError:
         raise UnsupportedFeature(f"MCNP Version {mcnp_version} is not supported.")
-
