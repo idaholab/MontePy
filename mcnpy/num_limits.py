4d8ed6aba2bde07629d72e3deb0f94a36ebbc2d9
Micah Gale <micah.gale@inl.gov>
2022-06-24 16:31:40 -0600

    Researched all the number limits.

---
+++ mcnpy/num_limits.py
@@ -0,0 +1,19 @@
+"""
+Maximum allowed numbers for object types
+"""
+
+_very_large_number = 99999999
+
+CELL_MAX_NUM = _very_large_number
+
+MATERIAL_MAX_NUM = _very_large_number
+
+SURFACES_MAX_NUM = _very_large_number
+
+SURFACES_WITH_TRANSFORM_MAX_NUM = 999
+
+TRANSFORM_MAX_NUM = 999
+
+UNIVERSE_MAX_NUM = _very_large_number
+
+TALLY_MAX_NUM = _very_large_number
