206ded9628effee416499de27408d89c1744e234
Micah Gale <micah.gale@inl.gov>
2022-06-29 13:58:00 -0600

    Made shell class for now

---
+++ mcnpy/data_cards/cell_modifier.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod
+from mcnpy.data_cards.data_card import DataCardAbstract
+
+
+class CellModifierCard(DataCardAbstract):
+    """
+    Abstract Parent class for Data Cards that modify cells/ geometry.
+
+    Examples: IMP, VOL, etc.
+    """
+
+    pass
