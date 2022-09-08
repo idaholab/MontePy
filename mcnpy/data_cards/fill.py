fa146a715226be16c75f72b7f6002d160bfbd603
Micah D. Gale <micah.gale@inl.gov>
2022-09-08 10:03:46 -0600

    Worked on parsing fill cards

--- mcnpy/data_cards/fill.py
+++ mcnpy/data_cards/fill.py
@@ -30,6 +30,58 @@ class Fill(CellModifierCard):
         super().__init__(input_card, comments, in_cell_block, key, value)
         self._old_number = None
         self._universe = None
+        self._transform = None
+        if self.in_cell_block:
+            if key:
+                self._parse_cell_input(value)
+        elif input_card:
+            self._old_number = []
+            words = self.words[1:]
+            for word in words:
+                if isinstance(word, str):
+                    try:
+                        value = int(word)
+                        assert value >= 0
+                        self._old_number.append(value)
+                    except (ValueError, AssertionError) as e:
+                        raise MalformedInputError(
+                            input_card,
+                            f"Cell fill must be set to a valid universe, {word} was given",
+                        )
+                elif isinstance(word, Jump):
+                    self._old_number.append(word)
+                else:
+                    raise TypeError(
+                        f"Word: {word} cannot be parsed as a lattice as a str, or Jump"
+                    )
+
+    def _parse_cell_input(self, value):
+        def get_universe(value):
+            words = value.split()
+            try:
+                value = int(words[0])
+                assert value > 0
+            except (ValueError, AssertionError) as e:
+                raise ValueError(
+                    f"The fill universe must be a valid integer, {words[0]} was given"
+                )
+
+        if "(" in value:
+            get_universe(value)
+            parens_contents = value[value.index("(") + 1 : value.rindex(")")]
+            words = parens_contents.split()
+            if len(words) == 1:
+                try:
+                    transform = int(words[0])
+                    assert transform > 0
+                except (ValueError, AssertionError) as e:
+                    raise ValueError(
+                        "Transform number must be a positive integer. {words[0]} was given."
+                    )
+        elif ":" in value:
+            pass
+        else:
+            get_universe(value)
 
     @property
     def class_prefix(self):
