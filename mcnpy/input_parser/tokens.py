7e237289519acd8dcaf17036500e513bec5c625a
Micah D. Gale <micah.gale@inl.gov>
2022-11-28 14:32:36 -0700

    Named token classes

---
+++ mcnpy/input_parser/tokens.py
@@ -0,0 +1,28 @@
+from mcnpy.input_parser.mcnp_input import SyntaxNode
+
+class Token(SyntaxNode):
+    """
+    Class to represent a syntax Token
+    """
+    pass
+
+
+class IdentifierToken(Token):
+    """
+    Class to represent a Identifier Token.
+
+    Object identifiers (an object number).
+    """
+    pass
+
+class LiteralToken(Token):
+    """
+    Class to represent a literal token providing data.
+    """
+    pass
+
+class CommentToken(Token):
+    """
+    Class to represent a token in a comment.
+    """
+    pass
