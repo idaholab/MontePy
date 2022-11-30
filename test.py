0b9e6aa84f42a133c41cec61c3bd9de2251a8619
Micah D. Gale <micah.gale@inl.gov>
2022-11-30 12:34:55 -0700

    Made toy tester more thurough

--- test.py
+++ test.py
@@ -3,7 +3,17 @@ import mcnpy
 Input = mcnpy.input_parser.mcnp_input.Input
 tokens = mcnpy.input_parser.tokens
 
-input = Input(["line 1", "line  2"], mcnpy.input_parser.block_type.BlockType.CELL)
-
+input = Input(
+    [
+        "line 1",
+        "line  2",
+        "     line 3 $ hi",
+        "C this is a comment",
+        "cnot comment",
+        "c",
+    ],
+    mcnpy.input_parser.block_type.BlockType.CELL,
+)
+print(input.input_lines)
 for token in tokens.tokenize(input):
-    print(token.original_input)
+    print(type(token), token.original_input.encode())
