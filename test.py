1ba3f07f286169c2b858b5a679475e91eec46892
Micah D. Gale <micah.gale@inl.gov>
2022-12-06 13:49:00 -0700

    Updated test to be more accurate.
    -Made node for every logical thing
    -added more complex comments to parse

--- test.py
+++ test.py
@@ -14,19 +14,41 @@ Input = mcnpy.input_parser.mcnp_input.Input
 
 input = Input(
     [
-        "5 10 -0.5",
+        "5  $foo 10 -0.5",
+        "    10 -0.5",
         "C this is a comment",
     ],
     mcnpy.input_parser.block_type.BlockType.CELL,
 )
 cell_parser = NodeParser(
     [1],
+    "cell",
     children=[
-        TokenParser(IdentifierToken, "_old_number"),
-        TokenParser(SeperatorToken),
-        TokenParser(IdentifierToken, "_material"),
-        TokenParser(SeperatorToken),
-        NodeParser({0, 1}, [TokenParser(LiteralToken, "_density")]),
+        NodeParser(
+            [1],
+            "cell number",
+            children=[
+                TokenParser(IdentifierToken, map_to="_old_number"),
+                TokenParser(SeperatorToken),
+            ],
+        ),
+        NodeParser(
+            [1],
+            "material",
+            children=[
+                TokenParser(IdentifierToken, map_to="_material"),
+                TokenParser(SeperatorToken),
+            ],
+        ),
+        NodeParser(
+            {0, 1},
+            "density",
+            children=[
+                TokenParser(LiteralToken, map_to="_density"),
+                TokenParser(SeperatorToken),
+            ],
+        ),
     ],
 )
 cell_parser.parse(input)
+print(cell_parser.print_nodes())
