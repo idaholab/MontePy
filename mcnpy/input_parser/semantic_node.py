170d575e8d0d32a50bf0795bb8a46e0804d5c3c9
Micah D. Gale <micah.gale@inl.gov>
2022-12-07 17:03:50 -0700

    Made method to find the last leaf parent node

--- mcnpy/input_parser/semantic_node.py
+++ mcnpy/input_parser/semantic_node.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from mcnpy.input_parser.tokens import Token
 
 
 class SemanticNode(ABC):
@@ -10,10 +11,28 @@ class SemanticNode(ABC):
         # todo type checking
         self._nodes.append(node)
 
+    @property
+    def nodes(self):
+        return self._nodes
+
     def has_leaves(self):
+        if any([isinstance(x, Token) for x in self.nodes]):
+            return True
         for node in self.nodes:
-            pass
+            if isinstance(node, SemanticNode):
+                if node.has_leaves:
+                    return True
         return False
+
+    def get_last_leaf_parent(self):
+        for node in self.nodes[::-1]:
+            if isinstance(node, Token):
+                return self
+            if node.has_leaves:
+                return node.get_last_leaf_parent()
+
+    def __len__(self):
+        return len(self.nodes)
 
     def print_nodes(self):
         ret = []
