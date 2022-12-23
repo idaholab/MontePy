481ff1f31068f85b4227f3c68a44b024363aca5c
Micah D. Gale <micah.gale@inl.gov>
2022-12-23 09:02:52 -0700

    Made input text property for tokenizing

--- mcnpy/input_parser/mcnp_input.py
+++ mcnpy/input_parser/mcnp_input.py
@@ -63,6 +63,10 @@ class SyntaxNode(ABC):
         return self._input_lines
 
     @property
+    def input_text(self):
+        return "\n".join(self.input_lines)
+
+    @property
     def mutated(self):
         """If true this input has been mutated by the user, and needs to be formatted
 
@@ -140,26 +144,6 @@ class Input(SyntaxNode):
 
     def format_for_mcnp_input(self, mcnp_version):
         pass
-
-    def __iter__(self):
-        return InputIterator(self.input_lines)
-
-
-class InputIterator:
-    def __init__(self, lines):
-        self._lines = lines
-        self._line_iter = iter(self._lines)
-        self._iter = self.get_next_line()
-
-    def get_next_line(self):
-        return iter(next(self._line_iter) + "\n")
-
-    def __next__(self):
-        try:
-            return next(self._iter)
-        except StopIteration:
-            self._iter = self.get_next_line()
-            return next(self._iter)
 
 
 def parse_input_shortcuts(words, input=None):
--- test.py
+++ test.py
@@ -7,12 +7,11 @@ Input = mcnpy.input_parser.mcnp_input.Input
 input = Input(
     [
         "5  $foo 10 -0.5",
-        "     1 0.5e-10 ",
+        "     1 0.5 ",
+        "     ( 1 2 3)",
         "C this is a comment",
     ],
     mcnpy.input_parser.block_type.BlockType.CELL,
 )
-for token in mcnpy.input_parser.tokens.MCNP_Lexer().tokenize(
-    "\n".join(input.input_lines)
-):
+for token in mcnpy.input_parser.tokens.MCNP_Lexer().tokenize(input.input_text):
     print(token)
