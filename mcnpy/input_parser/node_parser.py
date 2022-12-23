13499b421064cd43e383715cc3bdcd8b48d6c2b3
Micah D. Gale <micah.gale@inl.gov>
2022-12-23 15:39:08 -0700

    Prototyped system for parser subclassing

--- mcnpy/input_parser/node_parser.py
+++ mcnpy/input_parser/node_parser.py
@@ -1,22 +1,39 @@
 from mcnpy.input_parser.tokens import MCNP_Lexer
 from sly import Parser
+import sly
+
+_dec = sly.yacc._decorator
 
 
-class CellParser(Parser):
+class MetaBuilder(sly.yacc.ParserMeta):
+    protected_names = {
+        "debugfile",
+        "errok",
+        "error",
+        "index_position",
+        "line_position",
+        "log",
+        "parse",
+        "restart",
+        "tokens",
+    }
+
+    def __new__(meta, classname, bases, attributes):
+        if classname != "MCNP_Parser":
+            for attr_name in dir(MCNP_Parser):
+                if (
+                    not attr_name.startswith("_")
+                    and attr_name not in MetaBuilder.protected_names
+                ):
+                    print(attr_name)
+                    func = getattr(MCNP_Parser, attr_name)
+                    attributes[attr_name] = func
+        cls = super().__new__(meta, classname, bases, attributes)
+        return cls
+
+
+class MCNP_Parser(Parser, metaclass=MetaBuilder):
     tokens = MCNP_Lexer.tokens
-    debugfile = "parser.out"
-
-    @_("int_phrase material")
-    def cell(self, p):
-        return p
-
-    @_("NULL SPACE")
-    def material(self, p):
-        return p
-
-    @_("int_phrase float_phrase")
-    def material(self, p):
-        return p
 
     @_("INT padding")
     def int_phrase(self, p):
@@ -40,4 +57,22 @@ class CellParser(Parser):
 
     @_("padding COMMENT")
     def padding(self, p):
+        return p
+
+    @_("padding #")
+    def padding(self, p):
+        return p
+
+
+class CellParser(MCNP_Parser):
+    @_("int_phrase material")
+    def cell(self, p):
+        return p
+
+    @_("NULL SPACE")
+    def material(self, p):
+        return p
+
+    @_("int_phrase float_phrase")
+    def material(self, p):
         return p
