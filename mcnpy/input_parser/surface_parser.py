b8e117f54cbf893f977fb68227506ccdfac725ed
Micah D. Gale <micah.gale@inl.gov>
2023-01-06 09:01:51 -0700

    Made surface parser handle transforms, and modifier literals

--- mcnpy/input_parser/surface_parser.py
+++ mcnpy/input_parser/surface_parser.py
@@ -2,6 +2,14 @@ from mcnpy.input_parser.parser_base import MCNP_Parser
 
 
 class SurfaceParser(MCNP_Parser):
-    @_("number_phrase SURFACE_TYPE padding number_sequence")
+    @_("surface_id SURFACE_TYPE padding number_sequence")
     def surface(self, p):
+        return p
+
+    @_("surface_id number_phrase SURFACE_TYPE padding number_sequence")
+    def surface(self, p):
+        return p
+
+    @_('"*" number_phrase', '"+" number_phrase', "number_phrase")
+    def surface_id(self, p):
         return p
