41df8b1939b9664e53f616a08e5deaf3a52dbf09
Micah D. Gale <micah.gale@inl.gov>
2023-01-06 08:42:39 -0700

    Made new system to identify keywords

--- mcnpy/input_parser/tokens.py
+++ mcnpy/input_parser/tokens.py
@@ -21,6 +21,70 @@ class MCNP_Lexer(Lexer):
         LOG_INTERPOLATE,
         PARTICLE_DESIGNATOR,
         KEYWORD,
+        SURFACE_TYPE,
+    }
+    _KEYWORDS = {
+        "imp",
+        "vol",
+        "pwt",
+        "ext",
+        "fcl",
+        "wwn",
+        "dxc",
+        "nonu",
+        "pd",
+        "tmp",
+        "u",
+        "trcl",
+        "lat",
+        "fill",
+        "elpt",
+        "cosy",
+        "bflcl",
+        "unc",
+    }
+
+    _SURFACE_TYPES = {
+        "p",
+        "px",
+        "py",
+        "pz",
+        "so",
+        "s",
+        "sx",
+        "sy",
+        "sz",
+        "c/x",
+        "c/y",
+        "c/z",
+        "cx",
+        "cy",
+        "cz",
+        "k/x",
+        "k/y",
+        "k/z",
+        "kx",
+        "ky",
+        "kz",
+        "sq",
+        "gq",
+        "tx",
+        "ty",
+        "tz",
+        "x",
+        "y",
+        "z",
+        "box",
+        "rpp",
+        "sph",
+        "rcc",
+        "rhp",
+        "hex",
+        "rec",
+        "trc",
+        "ell",
+        "wed",
+        "arb",
     }
 
     literals = {"(", ":", ")", "&", "#", "="}
@@ -84,31 +148,7 @@ class MCNP_Lexer(Lexer):
         self.lineno += t.value.count("\n")
         return t
 
-    @_(
-        r"""
-       imp|
-       vol|
-       pwt|
-       ext|
-       fcl|
-       wwn|
-       dxc|
-       nonu|
-       pd|
-       tmp|
-       u|
-       trcl|
-       lat|
-       fill|
-       elpt|
-       cosy|
-       bflcl|
-       unc"""
-    )
-    def KEYWORD(self, t):
-        return t
-
-    @_(r"\d*R")
+    @_(r"\d*R\s")
     def REPEAT(self, t):
         try:
             t.repeat_num = int(t.value.lower().replace("r", ""))
@@ -116,7 +156,7 @@ class MCNP_Lexer(Lexer):
             t.repeat_num = 1
         return t
 
-    @_(r"\d*M")
+    @_(r"\d*M\s")
     def MULTIPLY(self, t):
         try:
             t.multiply_num = int(t.value.lower().replace("m", ""))
@@ -124,7 +164,7 @@ class MCNP_Lexer(Lexer):
             t.multiply_num = 1
         return t
 
-    @_(r"\d*I")
+    @_(r"\d*I\s")
     def INTERPOLATE(self, t):
         try:
             t.interp_num = int(t.value.lower().replace("i", ""))
@@ -132,7 +172,7 @@ class MCNP_Lexer(Lexer):
             t.interp_num = 1
         return t
 
-    @_(r"\d*J")
+    @_(r"\d*J\s")
     def JUMP(self, t):
         try:
             t.jump_num = int(t.value.lower().replace("i", ""))
@@ -140,13 +180,25 @@ class MCNP_Lexer(Lexer):
             t.jump_num = 1
         return t
 
-    @_(r"\d*I?LOG")
+    @_(r"\d*I?LOG\s")
     def LOG_INTERPOLATE(self, t):
         try:
             t.jump_num = int(t.value.lower().replace("i", ""))
         except ValueError:
             t.jump_num = 1
         return t
+
+    @_(r"[a-z/]+")
+    def TEXT(self, t):
+        if t.value.lower() in self._KEYWORDS:
+            t.type = "KEYWORD"
+        elif t.value.lower() in self._SURFACE_TYPES:
+            t.type = "SURFACE_TYPE"
+        return t
+
+    KEYWORD = r"imp"
+
+    SURFACE_TYPE = r"pz"
 
 
 def find_column(text, token):
