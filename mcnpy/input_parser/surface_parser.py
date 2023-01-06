7d6f9d18eac4d22b51e152bffca01590b7da25d0
Micah D. Gale <micah.gale@inl.gov>
2023-01-06 08:54:14 -0700

    Fixed surface parser by adding padding

--- mcnpy/input_parser/surface_parser.py
+++ mcnpy/input_parser/surface_parser.py
@@ -2,6 +2,6 @@ from mcnpy.input_parser.parser_base import MCNP_Parser
 
 
 class SurfaceParser(MCNP_Parser):
-    @_("number_phrase SURFACE_TYPE number_sequence")
+    @_("number_phrase SURFACE_TYPE padding number_sequence")
     def surface(self, p):
         return p
