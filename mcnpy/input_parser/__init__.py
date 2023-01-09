b709dc298674cafab5f5119018989d9f0dc6648f
Micah D. Gale <micah.gale@inl.gov>
2023-01-09 07:49:55 -0700

    Added base material parser

--- mcnpy/input_parser/__init__.py
+++ mcnpy/input_parser/__init__.py
@@ -3,6 +3,7 @@ from . import block_type
 from . import cell_parser
 from . import constants
 from . import input_reader
+from . import material_parser
 from . import mcnp_input
 from . import parser_base
 from . import surface_parser
---
+++ mcnpy/input_parser/material_parser.py
@@ -0,0 +1,24 @@
+from mcnpy.input_parser.parser_base import MCNP_Parser
+
+
+class MaterialParser(MCNP_Parser):
+    @_("identifier isotope_fractions")
+    def material(self, p):
+        return p
+
+    @_("TEXT NUMBER padding")
+    def identifier(self, p):
+        if p.TEXT.lower() == "m":
+            return p
+
+    @_("ZAID padding")
+    def zaid_phrase(self, p):
+        return p
+
+    @_("zaid_phrase number_phrase")
+    def isotope_fraction(self, p):
+        return p
+
+    @_("isotope_fraction", "isotope_fractions isotope_fraction")
+    def isotope_fractions(self, p):
+        return p
