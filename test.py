9ac92612e65cf9a90bfeabbe68ab1f730405b585
Micah D. Gale <micah.gale@inl.gov>
2023-01-09 15:31:53 -0700

    Made harder read

--- test.py
+++ test.py
@@ -4,11 +4,9 @@ import mcnpy
 Input = mcnpy.input_parser.mcnp_input.Input
 
 input = Input(
-    ["read file=foo.imcnp"],
+    ["read $hi", "    file=foo.imcnp encode=hi"],
     mcnpy.input_parser.block_type.BlockType.CELL,
 )
 lexer = mcnpy.input_parser.tokens.MCNP_Lexer()
 parser = mcnpy.input_parser.read_parser.ReadParser()
-for token in lexer.tokenize(input.input_text):
-    print(token)
 print(parser.parse(lexer.tokenize(input.input_text)))
