f69e19a5ba217bcbcc1d8db71958b43b04d3fd79
Micah D. Gale <micah.gale@inl.gov>
2023-01-09 15:36:49 -0700

    Removed useless seen action

--- mcnpy/input_parser/read_parser.py
+++ mcnpy/input_parser/read_parser.py
@@ -2,9 +2,9 @@ from mcnpy.input_parser.parser_base import MCNP_Parser
 
 
 class ReadParser(MCNP_Parser):
-    @_("KEYWORD seen_keyword padding parameters")
-    def read(self, p):
-        if p.KEYWORD.value.lower() == "read":
+    @_("KEYWORD padding parameters")
+    def read_input(self, p):
+        if p.KEYWORD.lower() == "read":
             return p
         else:
             return False
