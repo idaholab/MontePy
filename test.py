edfa385e582dfa5a2d3cff20235009e1803b1c86
Micah D. Gale <micah.gale@inl.gov>
2023-01-09 07:50:12 -0700

    Wrote material testing

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ Input = mcnpy.input_parser.mcnp_input.Input
 
 ""
 input = Input(
-    ["5 CZ 5.0 3.0"],
+    ["m1 1001.80c 0.6 8016.80c 0.4 plib=80c"],
     mcnpy.input_parser.block_type.BlockType.CELL,
 )
 lexer = mcnpy.input_parser.tokens.MCNP_Lexer()
-parser = mcnpy.input_parser.surface_parser.SurfaceParser()
+parser = mcnpy.input_parser.material_parser.MaterialParser()
 for token in lexer.tokenize(input.input_text):
     print(token)
 print(parser.parse(lexer.tokenize(input.input_text)))
