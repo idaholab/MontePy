0102e4f7bf104fc14b0e11c922c448cd581bbf0a
Micah D. Gale <micah.gale@inl.gov>
2023-01-10 15:50:58 -0700

    Generated geometry tree node

--- mcnpy/input_parser/cell_parser.py
+++ mcnpy/input_parser/cell_parser.py
@@ -27,19 +27,28 @@ class CellParser(MCNP_Parser):
 
     @_('":"')
     def union(self, p):
-        return p
+        return semantic_node.PaddingNode(p[0])
 
     @_("union padding")
     def union(self, p):
-        return p
+        ret = p.union
+        for node in p.padding.nodes:
+            ret.append(node)
+        return ret
 
     @_("geometry_expr union geometry_term")
     def geometry_expr(self, p):
-        return p
+        left = p.geometry_expr
+        right = p.geometry_term
+        return semantic_node.GeometryTree(
+            "union", left.nodes + [p.union] + right.nodes, ":", left, right
+        )
 
     @_("geometry_expr padding")
     def geometry_expr(self, p):
-        return p
+        ret = p.geometry_expr
+        ret.append(p.padding)
+        return ret
 
     @_("geometry_term")
     def geometry_expr(self, p):
@@ -47,11 +56,17 @@ class CellParser(MCNP_Parser):
 
     @_("geometry_term padding geometry_factor")
     def geometry_term(self, p):
-        return p
+        left = p.geometry_term
+        right = p.geometry_factor
+        return semantic_node.GeometryTree(
+            "intersection", left.nodes + [p.padding] + right.nodes, "*", left, right
+        )
 
     @_("geometry_term padding")
     def geometry_term(self, p):
-        return p
+        ret = p.geometry_term
+        ret.append(p.padding)
+        return ret
 
     @_("geometry_factor")
     def geometry_term(self, p):
@@ -63,15 +78,25 @@ class CellParser(MCNP_Parser):
 
     @_("COMPLEMENT geometry_factory")
     def geometry_factor(self, p):
-        return p
+        return semantic_node.GeometryTree(
+            "complement",
+            [p.COMPLEMENT] + p.geometry_factory.nodes,
+            "#",
+            p.geometry_factory,
+        )
 
     @_("NUMBER")
     def geometry_factory(self, p):
-        return p
+        return semantic_node.ValueNode(p.NUMBER, float)
 
     @_('"(" geometry_expr ")"')
     def geometry_factory(self, p):
-        return p
+        ret = semantic_node.ListNode("geom parens")
+        ret.append(p[0])
+        for node in p.geometry_expr.nodes:
+            ret.append(node)
+        ret.append(p[2])
+        return ret
 
     @_("KEYWORD PARTICLE_DESIGNATOR param_seperator number_phrase")
     def parameter(self, p):
