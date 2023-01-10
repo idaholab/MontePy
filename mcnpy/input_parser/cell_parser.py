e464c8cdb0dacf060aec9d524ed80466f25f5c44
Micah D. Gale <micah.gale@inl.gov>
2023-01-10 09:39:01 -0700

    Made cell parser return semantic nodes

--- mcnpy/input_parser/cell_parser.py
+++ mcnpy/input_parser/cell_parser.py
@@ -1,22 +1,29 @@
 from mcnpy.input_parser.parser_base import MCNP_Parser
+from mcnpy.input_parser import semantic_node
 
 
 class CellParser(MCNP_Parser):
-    @_("number_phrase material geometry_expr parameters")
+    @_(
+        "number_phrase material geometry_expr parameters",
+        "number_phrase material geometry_expr",
+    )
     def cell(self, p):
-        return p
+        dict_tree = {
+            "cell_num": p.number_phrase,
+            "material": p.material,
+            "geometry": p.geometry_expr,
+        }
+        if len(p) == 4:
+            dict_tree["parameters"] = p.parameters
+        return semantic_node.SemanticNode("cell", dict_tree)
 
-    @_("number_phrase material geometry_expr")
-    def cell(self, p):
-        return p
-
-    @_("null_phrase")
+    @_("null_phrase", "number_phrase number_phrase")
     def material(self, p):
-        return p
-
-    @_("number_phrase number_phrase")
-    def material(self, p):
-        return p
+        if len(p) == 1:
+            ret_dict = {"mat_number": p.null_phrase}
+        else:
+            ret_dict = {"mat_number": p[0], "density": p[1]}
+        return semantic_node.SemanticNode("material", ret_dict)
 
     @_('":"')
     def union(self, p):
