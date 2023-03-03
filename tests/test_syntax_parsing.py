8938e2a8f455c302d3dd0044b92671b806f5e808
Micah D. Gale <micah.gale@inl.gov>
2023-03-03 13:33:46 -0700

    Added test for hops and jumps

--- tests/test_syntax_parsing.py
+++ tests/test_syntax_parsing.py
@@ -333,6 +333,13 @@ bar
         with self.assertRaises(TypeError):
             bool(jump)
 
+    def test_jump_and_a_hop(self):
+        jump = Jump()
+        # first you need to hop
+        self.assertEqual("j", jump.lower())
+        # before you can jump
+        self.assertEqual("J", jump.upper())
+
 
 class DataCardTestFixture(mcnpy.data_cards.data_card.DataCardAbstract):
     def __init__(self, input_card=None, comment=None):
