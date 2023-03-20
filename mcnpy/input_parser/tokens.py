6987f24883343c0240089c5581504cb53db34886
Micah D. Gale <micah.gale@inl.gov>
2023-03-20 11:03:21 -0600

    Pushed all new text RE out.

--- mcnpy/input_parser/tokens.py
+++ mcnpy/input_parser/tokens.py
@@ -245,7 +245,7 @@ class ParticleLexer(MCNP_Lexer):
     def PARTICLE_DESIGNATOR(self, t):
         return t
 
-    @_(r"[+\-]?[0-9]*\.?[0-9]*E?[+\-]?[0-9]*[a-df-z]+[a-z\./]*")
+    @_(r"([+\-]?[0-9]*\.?[0-9]*E?[+\-]?[0-9]*[ijrml]+[a-z\./]*)|([a-z\./]+)")
     def TEXT(self, t):
         t = super().TEXT(t)
         if t.value.lower() in self._KEYWORDS:
@@ -371,7 +371,7 @@ class SurfaceLexer(MCNP_Lexer):
         "arb",
     }
 
-    @_(r"[+\-]?[0-9]*\.?[0-9]*E?[+\-]?[0-9]*[a-df-z]+[a-z\./]*")
+    @_(r"([+\-]?[0-9]*\.?[0-9]*E?[+\-]?[0-9]*[ijrml]+[a-z\./]*)|([a-z\./]+)")
     def TEXT(self, t):
         t = super().TEXT(t)
         if t.value.lower() in self._SURFACE_TYPES:
