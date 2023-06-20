1d921b3876c35e0026dbd512ace7b544d074bab8
Micah D. Gale <micah.gale@inl.gov>
2023-06-20 08:31:01 -0500

    Updated fill has_information test to handle problem not implemented.

--- mcnpy/data_inputs/fill.py
+++ mcnpy/data_inputs/fill.py
@@ -342,7 +342,12 @@ class Fill(CellModifierInput):
     @property
     def has_information(self):
         if self.in_cell_block:
-            return self.universe is not None or self.universes is not None
+            return (
+                self.universe is not None
+                or self.old_universe_number is not None
+                or self.universes is not None
+                or self.old_universe_numbers is not None
+            )
 
     @property
     def _tree_value(self):
