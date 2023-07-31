274bf3c5f3cc8b2a216878bead1cf5da0c9f07de
Micah D. Gale <micah.gale@inl.gov>
2023-07-31 11:32:42 -0500

    Updated tally parsing test.
    1. used data parser to invoke parser functions
    2. Switched tally example to most complicated case possible.
    3. Switched prefixes to lower case because I read the docs.

--- tests/test_tally.py
+++ tests/test_tally.py
@@ -1,5 +1,5 @@
 import mcnpy
-from mcnpy.data_inputs.data_input import DataInput
+from mcnpy.data_inputs.data_parser import parse_data
 from mcnpy.input_parser.block_type import BlockType
 from mcnpy.input_parser.mcnp_input import Input
 
@@ -9,11 +9,11 @@ from unittest import TestCase
 
 class TestTallyParser(TestCase):
     def test_parsing_tally_groups(self):
-        input = Input(["F4 (1 3i 5)"], BlockType.DATA)
-        data = DataInput(input)
-        self.assertEqual(data.prefix, "F")
+        input = Input(["F4:n (1 3i 5) T"], BlockType.DATA)
+        data = parse_data(input)
+        self.assertEqual(data.prefix, "f")
 
     def test_parsing_tally_print(self):
         input = Input(["Fq4 f p e"], BlockType.DATA)
-        data = DataInput(input)
-        self.assertEqual(data.prefix, "FQ")
+        data = parse_data(input)
+        self.assertEqual(data.prefix, "fq")
