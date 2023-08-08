e1e4ba44c296b7903d534b7a6cebdebca48100a8
Micah D. Gale <micah.gale@inl.gov>
2023-08-08 09:16:50 -0500

    Tried to fix parsing rules, and broke everything for some reason.

--- mcnpy/input_parser/cell_parser.py
+++ mcnpy/input_parser/cell_parser.py
@@ -164,18 +164,30 @@ class CellParser(MCNP_Parser):
 
     # support for fill card weirdness
     @_(
-        'number_sequence "(" number_sequence ")"',
-        'number_sequence "(" number_sequence ")" padding',
+        "number_sequence number_group",
         'number_sequence ":" numerical_phrase',
         # support for TRCL syntax
-        '"(" number_sequence ")"',
+        "number_group",
     )
     def number_sequence(self, p):
-        if isinstance(p[0], str):
-            sequence = syntax_node.ListNode("parenthetical statement")
-            sequence.append(p[0])
-        else:
-            sequence = p[0]
+        sequence = p[0]
+        for node in list(p)[1:]:
+            if isinstance(node, syntax_node.ListNode):
+                for val in node.nodes:
+                    sequence.append(val)
+            elif isinstance(node, str):
+                sequence.append(syntax_node.PaddingNode(node))
+            else:
+                sequence.append(node)
+        return sequence
+
+    @_(
+        '"(" number_sequence ")"',
+        '"(" number_sequence ")" padding',
+    )
+    def number_group(self, p):
+        sequence = syntax_node.ListNode("parenthetical statement")
+        sequence.append(p[0])
         for node in list(p)[1:]:
             if isinstance(node, syntax_node.ListNode):
                 for val in node.nodes:
