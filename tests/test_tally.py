94845bce5d66f046ba4f32f7c6431f1d245c3278
Micah D. Gale <micah.gale@inl.gov>
2023-08-28 17:28:24 -0500

    Made tally test more DRY.

--- tests/test_tally.py
+++ tests/test_tally.py
@@ -9,21 +9,19 @@ from unittest import TestCase
 
 class TestTallyParser(TestCase):
     def test_parsing_tally_groups(self):
-        input = Input(["F4:n (1 3i 5) T"], BlockType.DATA)
-        data = parse_data(input)
-        self.assertEqual(data.prefix, "f")
-        input = Input(["F4:n 1 2 3"], BlockType.DATA)
-        data = parse_data(input)
-        self.assertEqual(data.prefix, "f")
-        input = Input(["F4:n (1 3i 5) (7 8 9) T"], BlockType.DATA)
-        data = parse_data(input)
-        self.assertEqual(data.prefix, "f")
-        input = Input(["F4:n (1 3i 5) (7 8 9)"], BlockType.DATA)
-        data = parse_data(input)
-        self.assertEqual(data.prefix, "f")
-        input = Input(["F7 (1 3i 5) (7 8 9)"], BlockType.DATA)
-        data = parse_data(input)
-        self.assertEqual(data.prefix, "f")
+        for line in [
+            "F4:n (1 3i 5) T",
+            "F4:n (1 2 3 4 5) T",
+            "F4:n 1 2 3",
+            "F4:n (1 3i 5) (7 8 9) T",
+            "f4:n (1 3i 5) (7 8 9)",
+            "F7 (1 3i 5) (7 8 9)",
+            "F7 (1 3i 5) (7 8 9) ",
+        ]:
+            print(line)
+            input = Input([line], BlockType.DATA)
+            data = parse_data(input)
+            self.assertEqual(data.prefix, "f")
 
     def test_parsing_tally_print(self):
         input = Input(["Fq4 f p e"], BlockType.DATA)
