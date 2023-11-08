785b645b1adc19e645dfb52b0b50dfed5b75625f
Micah D. Gale <micah.gale@inl.gov>
2023-11-08 07:24:35 -0600

    Moved all files and changed setup.py for rebrand
    Rebrand as requested by Los Alamos National Laboratory

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 from mcnpy import __version__, __author__, __email__, __maintainer__
 
 setup(
-    name="mcnpy",
+    name="montepy",
     version=__version__,
-    packages=find_packages(include=["mcnpy", "mcnpy.*"]),
-    description="A library for reading, editing, and writing MCNP input files",
+    packages=find_packages(include=["montepy", "montepy.*"]),
+    description="A library for reading, editing, and writing MCNP input files.",
     author=__author__,
     author_email=__email__,
     maintainer=__maintainer__,
     maintainer_email=__email__,
-    url="https://hpcgitlab.hpc.inl.gov/experiment_analysis/mcnpy/",
+    url="",
     install_requires=[
         "numpy",
     ],
