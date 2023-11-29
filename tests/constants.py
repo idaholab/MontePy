4dedec111f3c3575fc93523f6585e10b4725fec2
Micah D. Gale <micah.gale@inl.gov>
2023-11-29 08:42:03 -0600

    Moved file info to constants for tests.

---
+++ tests/constants.py
@@ -0,0 +1,19 @@
+BAD_INPUTS = {
+    "test_excess_mt.imcnp",
+    "test_vol_redundant.imcnp",
+    "testVerticalMode.imcnp",
+    "test_missing_mat_for_mt.imcnp",
+    "test_imp_redundant.imcnp",
+    "test_broken_cell_surf_link.imcnp",
+    "test_broken_surf_link.imcnp",
+    "test_broken_transform_link.imcnp",
+    "test_broken_mat_link.imcnp",
+    "test_broken_complement.imcnp",
+    "testVerticalMode.imcnp",
+}
+IGNORE_FILES = {
+    "testReadRec1.imcnp",
+    "testReadRec2.imcnp",
+    "testReadRec3.imcnp",
+    "testReadTarget.imcnp",
+}
