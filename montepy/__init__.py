e08b8b277a91cdef178ca04407ab4079a7d437a8
Micah D. Gale <micah.gale@inl.gov>
2024-01-03 17:09:22 -0600

    actually reved version.

--- montepy/__init__.py
+++ montepy/__init__.py
@@ -23,7 +23,7 @@ from montepy.surfaces.surface_type import SurfaceType
 from montepy.universe import Universe
 import sys
 
-__version__ = "0.2.3dev3"
+__version__ = "0.2.3"
 # enable deprecated warnings for users
 if not sys.warnoptions:
     import os, warnings
