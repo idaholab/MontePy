a50203302e97922f757308c2c388a88df91db0f1
Micah D. Gale <mg@gale.engineer>
2024-01-13 22:08:51 -0600

    Added tallies object.

---
+++ montepy/tallies.py
@@ -0,0 +1,17 @@
+# Copyright 2024, Battelle Energy Alliance, LLC All Rights Reserved.
+import montepy
+from montepy.numbered_object_collection import NumberedObjectCollection
+
+Tally = montepy.data_inputs.tally.Tally
+
+
+class Tallies(NumberedObjectCollection):
+    """
+    A container of multiple :class:`~montepy.data_inputs.tally.Tally` instances.
+
+    :param objects: the list of tallies to start with if needed
+    :type objects: list
+    """
+
+    def __init__(self, objects=None, problem=None):
+        super().__init__(Tally, objects, problem)
