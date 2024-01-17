f488d48a8e222f95eecee6abb192f755e78c074b
Micah D. Gale <micah.gale@inl.gov>
2024-01-17 10:24:15 -0600

    Made dev version.

--- montepy/__init__.py
+++ montepy/__init__.py
@@ -23,7 +23,7 @@ from montepy.surfaces.surface_type import SurfaceType
 from montepy.universe import Universe
 import sys
 
-__version__ = "0.2.5"
+__version__ = "0.3.0dev1"
 
 # enable deprecated warnings for users
 if not sys.warnoptions:
