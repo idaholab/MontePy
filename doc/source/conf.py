b4abe8378a73ac352c86d61fd4fce90edca3d809
Travis J. Labossiere-Hickman <Travis.LabossiereHickman@INL.gov>
2024-06-18 14:08:33 -0600

    Add sphinx.ext.extlinks to Sphinx configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.extlinks",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
