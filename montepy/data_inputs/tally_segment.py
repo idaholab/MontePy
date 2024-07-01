f9e0d3c9ac715d8958fa3934bd302c83f9afd6b5
Micah D. Gale <micah.gale@inl.gov>
2024-07-01 13:28:02 -0500

    Made a simple tally segment parser.

--- montepy/data_inputs/tally_segment.py
+++ montepy/data_inputs/tally_segment.py
@@ -1,13 +1,13 @@
 # Copyright 2024, Battelle Energy Alliance, LLC All Rights Reserved.
 import montepy
 from montepy.data_inputs.data_input import DataInputAbstract
-from montepy.input_parser.tally_parser import TallyParser
+from montepy.input_parser.tally_seg_parser import TallySegmentParser
 
 
 class TallySegment(DataInputAbstract):
     """ """
 
-    _parser = TallyParser()
+    _parser = TallySegmentParser()
 
     @staticmethod
     def _class_prefix():
