05a9bcee64cd93db4a30891aac3611b7c9394911
Micah D. Gale <micah.gale@inl.gov>
2024-08-28 07:28:36 -0500

    Switched order of filters to make sense.

--- prof/dump_results.py
+++ prof/dump_results.py
@@ -2,5 +2,5 @@ import pstats
 from pstats import SortKey
 
 stats = pstats.Stats("prof/combined.prof")
-stats.sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats(300, "montepy")
-stats.sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats(100, "sly")
+stats.sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats("montepy", 50)
+stats.sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats("sly", 20)
