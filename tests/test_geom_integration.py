9ede62ea4291a8e51e808d2b968455387f1e20f5
Micah D. Gale <micah.gale@inl.gov>
2024-10-02 08:45:17 -0500

    Updated geometry test to replicate expected behavior.

--- tests/test_geom_integration.py
+++ tests/test_geom_integration.py
@@ -16,6 +16,9 @@ def test_build_arbitrary_cell_geometry(first_surf, new_surfaces):
         [f"1 0 {first_surf} imp:n=1"], montepy.input_parser.block_type.BlockType.CELL
     )
     cell = montepy.Cell(input)
+    surf = montepy.surfaces.surface.Surface()
+    surf.number = first_surf
+    cell.update_pointers([], [], montepy.surface_collection.Surfaces([surf]))
     for surf_num, operator in new_surfaces:
         surf = montepy.surfaces.surface.Surface()
         surf.number = surf_num
@@ -23,3 +26,15 @@ def test_build_arbitrary_cell_geometry(first_surf, new_surfaces):
             cell.geometry &= +surf
         else:
             cell.geometry |= -surf
+    assert len(cell.geometry) == len(new_surfaces) + 1
+
+
+def test_cell_geometry_set_warns():
+    input = montepy.input_parser.mcnp_input.Input(
+        [f"1 0 -1 imp:n=1"], montepy.input_parser.block_type.BlockType.CELL
+    )
+    cell = montepy.Cell(input)
+    with pytest.raises(montepy.errors.IllegalState):
+        surf = montepy.surfaces.surface.Surface()
+        surf.number = 5
+        cell.geometry &= +surf
