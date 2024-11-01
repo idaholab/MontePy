b5dc9b6d0465b9466ac0ce670e7f4c7ddebf620f
Micah Gale <mg@gale.engineer>
2024-11-01 13:44:25 -0500

    Added debugging info.

--- montepy/json.py
+++ montepy/json.py
@@ -5,8 +5,12 @@ from enum import Enum
 
 class MontepyJSONEncode(json.JSONEncoder):
     def default(self, o):
-        print(o)
+        try:
+            print(o)
+        except AttributeError:
+            pass
         data = o.__getstate__()
+        print(data)
         new_data = {}
         if not isinstance(data, dict):
             return data
