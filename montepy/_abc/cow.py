dbfca076886ddfb7f4ca642109c30d2bab7d0cdf
Micah D. Gale <micah.gale@inl.gov>
2025-04-10 11:57:03 -0500

    Got setattr working.

--- montepy/_abc/cow.py
+++ montepy/_abc/cow.py
@@ -16,15 +16,7 @@ class Moo:
             blank._heffer = self
         else:
             blank._heffer = self._heffer
-        # blank._make_cow()
         return blank
-
-    def _make_cow(self):
-        self.prepare_getter("__getattr__")
-        # self.prepare_setter("__setattr__")
-        if hasattr(self, "__getitem__"):
-            self.prepare_getter("__getitem__")
-            # self.prepare_setter("__setitem__")
 
     def __getattr__(self, key):
         if "__" in key or key == "_heffer":
@@ -34,20 +26,15 @@ class Moo:
         else:
             return super().__getattribute__(key)
 
-    def prepare_setter(self, attr_name):
-        def wrapper(self, key, value):
-            if key.startswith("_"):
-                getattr(self, attr_name)(key, value)
-            # TODO cowDict
-            # if isinstance(self, dict):
-            #    MalformedInputError
+    def __setattr__(self, key, value):
+        if key.startswith("_"):
+            super().__setattr__(key, value)
+        # TODO cowDict
+        # if isinstance(self, dict):
+        #    MalformedInputError
+        if hasattr(self, "_heffer") and self._heffer is not None:
             self.__dict__.update(copy.deepcopy(self._heffer.__dict__))
             del self._heffer
-            old_attr = getattr(self, f"_old{attr_name}")
-            if old_attr:
-                self.__dict__[attr_name] = getattr(self, f"_old{attr_name}")
-            else:
-                self.__dict__[attr_name] = getattr(super(), attr_name)
-            getattr(self, attr_name)(key, value)
+        super().__setattr__(key, value)
 
-        attributes[attr_name] = wrapper
+    # TODO delattr
