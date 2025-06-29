0c92c8df9ac1ee2f008bb9a7f181d44d68728b16
Micah Gale <mg@gale.engineer>
2025-06-29 10:48:20 -0500

    Refactored value checkers and added more.

--- montepy/_check_value.py
+++ montepy/_check_value.py
@@ -33,6 +33,7 @@ import os
 from collections.abc import Callable, Iterable
 import functools
 import inspect
+from numbers import Real, Integral
 import numpy as np
 import typing
 
@@ -411,7 +412,31 @@ def check_value(
         raise ValueError(msg)
 
 
-def less_than(maximum, equality=False):
+def less_than(maximum: Real, equality=False):
+    r"""
+    A higher-order function for use with ``args_checked`` to enforce a value being less than a value.
+
+
+    Examples
+    ^^^^^^^^
+    This can be used to annotate a value
+
+    .. testcode::
+
+        import numbers
+        import typing
+
+        @args_checked
+        def foo(a: typing.Annotated[numbers.Real, less_than(5)]):
+            pass
+
+    Parameters
+    ----------
+    maximum: Real
+        The maximum value to be tested against.
+    equality: bool
+        if true test for :math:`x\leq m` rather than :math:`x\lt m`
+    """
 
     def wrapper(func_name, name):
         return lambda x: check_less_than(func_name, name, x, maximum, equality)
@@ -419,27 +444,115 @@ def less_than(maximum, equality=False):
     return wrapper
 
 
-def greater_than(maximum, equality=False):
+def greater_than(minimum: Real, equality: bool = False):
+    r"""
+    A higher-order function for use with ``args_checked`` to enforce a value being greater than a value.
+
+
+    Examples
+    ^^^^^^^^
+    This can be used to annotate a value
+
+    .. testcode::
+
+        import numbers
+        import typing
+
+        @args_checked
+        def foo(a: typing.Annotated[numbers.Real, greater_than(5)]):
+            pass
+
+    Parameters
+    ----------
+    minimum: Real
+        The minimum value to be tested against.
+    equality: bool
+        if true test for :math:`x\geq m` rather than :math:`x\gt m`
+    """
 
     def wrapper(func_name, name):
-        return lambda x: check_less_than(func_name, name, x, maximum, equality)
+        return lambda x: check_greater_than(func_name, name, x, minimum, equality)
 
     return wrapper
 
 
 def positive(func_name, name):
+    r"""
+    A higher-order function to be used with ``args_checked`` to ensure a value is positive,
+    i.e., :math:`x\gt 0`.
+
+    Example
+    ^^^^^^^
+
+    .. testcode::
+
+        from numbers import Real
+        from typing import Annotated
+
+        def foo(a: Annotated[Real, positive]):
+            pass
+
+    """
     return lambda x: check_greater_than(func_name, name, x, 0)
 
 
-def negative():
+def negative(func_name, name):
+    r"""
+    A higher-order function to be used with ``args_checked`` to ensure a value is negative,
+    i.e., :math:`x\lt 0`.
+
+    Example
+    ^^^^^^^
+
+    .. testcode::
+
+        from numbers import Real
+        from typing import Annotated
+
+        def foo(a: Annotated[Real, negative]):
+            pass
+
+    """
     return lambda x: check_less_than(func_name, name, x, 0)
 
 
-def non_positive():
+def non_positive(func_name, name):
+    r"""
+    A higher-order function to be used with ``args_checked`` to ensure a value is non-positive,
+    i.e., :math:`x\leq 0`.
+
+    Example
+    ^^^^^^^
+
+    .. testcode::
+
+        from numbers import Real
+        from typing import Annotated
+
+        def foo(a: Annotated[Real, non_positive]):
+            pass
+
+    """
     return lambda x: check_less_than(func_name, name, x, 0, True)
 
 
-def non_negative():
+def non_negative(func_name, name):
+    r"""
+    A higher-order function to be used with ``args_checked`` to ensure a value is non-negative,
+    i.e., :math:`x\geq 0`.
+
+    Example
+    ^^^^^^^
+
+    .. testcode::
+
+        from numbers import Real
+        from typing import Annotated
+
+        def foo(a: Annotated[Real, non_negative]):
+            pass
+
+    """
     return lambda x: check_greater_than(func_name, name, x, 0, True)
 
 
