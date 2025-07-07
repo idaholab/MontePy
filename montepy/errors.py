d1493446883a0d0527d5a394c646e7852c9c3409
Travis J. Labossiere-Hickman <Travis.LabossiereHickman@INL.gov>
2025-07-07 13:32:50 -0600

    format with black

--- montepy/errors.py
+++ montepy/errors.py
@@ -3,10 +3,11 @@
 import warnings
 import montepy.exceptions
 
+
 def __getattr__(name):
     warnings.warn(
         message=f"montepy.errors.{name} is deprecated. Instead, use montepy.exceptions.{name}",
         category=DeprecationWarning,
-        stacklevel=2
+        stacklevel=2,
     )
     return getattr(montepy.exceptions, name)
