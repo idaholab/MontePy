11e4d87846f833f9acea587eec3aa33a614cd096
Travis J. Labossiere-Hickman <Travis.LabossiereHickman@INL.gov>
2025-07-07 14:18:53 -0600

    Work around spurious __warningregistry__ coverage

--- montepy/errors.py
+++ montepy/errors.py
@@ -5,9 +5,10 @@ import montepy.exceptions
 
 
 def __getattr__(name):
-    warnings.warn(
-        message=f"montepy.errors.{name} is deprecated. Instead, use montepy.exceptions.{name}",
-        category=DeprecationWarning,
-        stacklevel=2,
-    )
+    if not name.startswith("__"):  # ignore __warningregistry__ and other magic
+        warnings.warn(
+            message=f"montepy.errors.{name} is deprecated. Instead, use montepy.exceptions.{name}",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
     return getattr(montepy.exceptions, name)
