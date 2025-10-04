402cf55dce96e689a121c335e01aef9ad5cb7fc4
Micah D. Gale <mg@gale.engineer>
2025-10-03 22:51:32 -0500

    Fixed test nesting that Copilot screwed up.

--- tests/test_data_inputs.py
+++ tests/test_data_inputs.py
@@ -153,158 +153,152 @@ def test_volume_init_cell():
         )
         card = volume.Volume(key="VoL", value=node, in_cell_block=True)
 
-    # TODO merge manually
-    """
-    def test_volume_init_data(self):
-        in_str = "VOL 1 1 2J 0"
-        input_card = Card([in_str], BlockType.DATA)
+
+def test_volume_init_data():
+    vol_card = parse_data("VOL 1 1 2J 0")
+    answers = [1.0, 1.0, Jump, Jump, 0.0]
+    for i, vol in enumerate(vol_card._volume):
+        if isinstance(answers[i], float):
+            assert vol == pytest.approx(answers[i])
+        else:
+            assert isinstance(vol, Jump)
+    # test starting jump
+    vol_card = parse_data("VOL 2J 1 0")
+    answers = [Jump, Jump, 1.0, 0.0]
+    for i, vol in enumerate(vol_card._volume):
+        if isinstance(answers[i], float):
+            assert vol == pytest.approx(answers[i])
+        else:
+            assert isinstance(vol, Jump)
+    # tests starting no
+    vol_card = parse_data("VOL NO 1 1 2J 0")
+    assert not vol_card.is_mcnp_calculated
+    # invalid number
+    with self.assertRaises(MalformedInputError):
+        vol_card = parse_data("VOL NO s 1 2J 0")
+    # negative volume
+    with self.assertRaises(MalformedInputError):
+        vol_card = parse_data("VOL NO -1 1 2J 0")
+
+
+def test_volume_init_data():
+    in_str = "VOL 1 1 2J 0"
+    input_card = Input([in_str], BlockType.DATA)
+    vol_card = parse_data(input_card)
+    answers = [1.0, 1.0, None, None, 0.0]
+    for i, vol in enumerate(vol_card._volume):
+        if isinstance(vol, syntax_node.ValueNode):
+            assert vol.value == pytest.approx(answers[i])
+        else:
+            assert isinstance(vol, Jump)
+    in_str = "VOL NO 1 1 2J 0"
+    input_card = Input([in_str], BlockType.DATA)
+    vol_card = parse_data(input_card)
+    assert not vol_card.is_mcnp_calculated
+    # invalid number
+    in_str = "VOL NO s 1 2J 0"
+    input_card = Input([in_str], BlockType.DATA)
+    with pytest.raises(MalformedInputError):
         vol_card = parse_data(input_card)
-        answers = [1.0, 1.0, Jump, Jump, 0.0]
-        for i, vol in enumerate(vol_card._volume):
-            if isinstance(answers[i], float):
-                self.assertAlmostEqual(vol, answers[i])
-            else:
-                self.assertIsInstance(vol, Jump)
-        # test starting jump
-        in_str = "VOL 2J 1 0"
-        input_card = Card([in_str], BlockType.DATA)
+    # negative volume
+    in_str = "VOL NO -1 1 2J 0"
+    input_card = Input([in_str], BlockType.DATA)
+    with pytest.raises(MalformedInputError):
         vol_card = parse_data(input_card)
-        answers = [Jump, Jump, 1.0, 0.0]
-        for i, vol in enumerate(vol_card._volume):
-            if isinstance(answers[i], float):
-                self.assertAlmostEqual(vol, answers[i])
-            else:
-                self.assertIsInstance(vol, Jump)
-        # tests starting no
-        in_str = "VOL NO 1 1 2J 0"
-        input_card = Card([in_str], BlockType.DATA)
-        vol_card = parse_data(input_card)
-        self.assertTrue(not vol_card.is_mcnp_calculated)
-        # invalid number
-        in_str = "VOL NO s 1 2J 0"
-        input_card = Card([in_str], BlockType.DATA)
-        with self.assertRaises(MalformedInputError):
-            vol_card = parse_data(input_card)
-        # negative volume
-        in_str = "VOL NO -1 1 2J 0"
-        input_card = Card([in_str], BlockType.DATA)
-        with self.assertRaises(MalformedInputError):
-            vol_card = parse_data(input_card)
-    """
 
-    def test_volume_init_data(self):
-        in_str = "VOL 1 1 2J 0"
-        input_card = Input([in_str], BlockType.DATA)
-        vol_card = parse_data(input_card)
-        answers = [1.0, 1.0, None, None, 0.0]
-        for i, vol in enumerate(vol_card._volume):
-            if isinstance(vol, syntax_node.ValueNode):
-                self.assertAlmostEqual(vol.value, answers[i])
-            else:
-                self.assertIsInstance(vol, Jump)
-        in_str = "VOL NO 1 1 2J 0"
-        input_card = Input([in_str], BlockType.DATA)
-        vol_card = parse_data(input_card)
-        self.assertTrue(not vol_card.is_mcnp_calculated)
-        # invalid number
-        in_str = "VOL NO s 1 2J 0"
-        input_card = Input([in_str], BlockType.DATA)
-        with self.assertRaises(MalformedInputError):
-            vol_card = parse_data(input_card)
-        # negative volume
-        in_str = "VOL NO -1 1 2J 0"
-        input_card = Input([in_str], BlockType.DATA)
-        with self.assertRaises(MalformedInputError):
-            vol_card = parse_data(input_card)
 
-    def test_volumes_for_only_some_cells(self):
-        cells = [
-            montepy.Cell(Input([f"{i + 1} 0 -1 u=3"], BlockType.CELL))
-            for i in range(10)
-        ]
-        prob = MCNP_Problem(None)
-        prob.cells = cells
-        vol_card = Input(["VOL 1 1 2 3 5"], BlockType.DATA)
-        vol_data = volume.Volume(vol_card, in_cell_block=False)
-        vol_data.link_to_problem(prob)
-        vol_data.push_to_cells()
-        is_set = [cell.volume_is_set for cell in cells]
-        reference = [True] * 5 + [False] * 5
-        self.assertListEqual(is_set, reference)
+def test_volumes_for_only_some_cells():
+    cells = [
+        montepy.Cell(Input([f"{i + 1} 0 -1 u=3"], BlockType.CELL)) for i in range(10)
+    ]
+    prob = MCNP_Problem(None)
+    prob.cells = cells
+    vol_card = Input(["VOL 1 1 2 3 5"], BlockType.DATA)
+    vol_data = volume.Volume(vol_card, in_cell_block=False)
+    vol_data.link_to_problem(prob)
+    vol_data.push_to_cells()
+    is_set = [cell.volume_is_set for cell in cells]
+    reference = [True] * 5 + [False] * 5
+    assert is_set == reference
 
-    def test_volume_setter(self):
-        vol = 1.0
-        list_node = syntax_node.ListNode("data")
-        list_node.append(syntax_node.ValueNode(str(vol), float))
-        node = syntax_node.SyntaxNode(
-            "volume",
-            {
-                "classifier": syntax_node.ValueNode("VoL", str),
-                "seperator": syntax_node.ValueNode("=", str),
-                "data": list_node,
-            },
-        )
-        card = volume.Volume(key="VoL", value=node, in_cell_block=True)
-        card.volume = 5.0
-        self.assertEqual(card.volume, 5.0)
-        with self.assertRaises(TypeError):
-            card.volume = "hi"
-        with self.assertRaises(ValueError):
-            card.volume = -5.0
 
-    def test_volume_deleter(self):
-        vol = 1.0
-        list_node = syntax_node.ListNode("data")
-        list_node.append(syntax_node.ValueNode(str(vol), float))
-        node = syntax_node.SyntaxNode(
-            "volume",
-            {
-                "classifier": syntax_node.ValueNode("VoL", str),
-                "seperator": syntax_node.ValueNode("=", str),
-                "data": list_node,
-            },
-        )
-        card = volume.Volume(key="VoL", value=node, in_cell_block=True)
-        del card.volume
-        self.assertIsNone(card.volume)
+def test_volume_setter():
+    vol = 1.0
+    list_node = syntax_node.ListNode("data")
+    list_node.append(syntax_node.ValueNode(str(vol), float))
+    node = syntax_node.SyntaxNode(
+        "volume",
+        {
+            "classifier": syntax_node.ValueNode("VoL", str),
+            "seperator": syntax_node.ValueNode("=", str),
+            "data": list_node,
+        },
+    )
+    card = volume.Volume(key="VoL", value=node, in_cell_block=True)
+    card.volume = 5.0
+    assert card.volume == 5.0
+    with pytest.raises(TypeError):
+        card.volume = "hi"
+    with pytest.raises(ValueError):
+        card.volume = -5.0
 
-    def test_volume_merge(self):
-        vol = 1.0
-        list_node = syntax_node.ListNode("data")
-        list_node.append(syntax_node.ValueNode(str(vol), float))
-        node = syntax_node.SyntaxNode(
-            "volume",
-            {
-                "classifier": syntax_node.ValueNode("VoL", str),
-                "seperator": syntax_node.ValueNode("=", str),
-                "data": list_node,
-            },
-        )
-        card = volume.Volume(key="VoL", value=node, in_cell_block=True)
-        card2 = volume.Volume(key="VoL", value=node, in_cell_block=True)
-        with self.assertRaises(MalformedInputError):
-            card.merge(card2)
 
-    def test_volume_repr(self):
-        vol = 1.0
-        list_node = syntax_node.ListNode("data")
-        list_node.append(syntax_node.ValueNode(str(vol), float))
-        node = syntax_node.SyntaxNode(
-            "volume",
-            {
-                "classifier": syntax_node.ValueNode("VoL", str),
-                "seperator": syntax_node.ValueNode("=", str),
-                "data": list_node,
-            },
-        )
-        card = volume.Volume(key="VoL", value=node, in_cell_block=True)
-        self.assertIn("VOLUME", repr(card))
+def test_volume_deleter():
+    vol = 1.0
+    list_node = syntax_node.ListNode("data")
+    list_node.append(syntax_node.ValueNode(str(vol), float))
+    node = syntax_node.SyntaxNode(
+        "volume",
+        {
+            "classifier": syntax_node.ValueNode("VoL", str),
+            "seperator": syntax_node.ValueNode("=", str),
+            "data": list_node,
+        },
+    )
+    card = volume.Volume(key="VoL", value=node, in_cell_block=True)
+    del card.volume
+    assert card.volume is None
 
-    def test_data_clone(_):
-        problem = montepy.MCNP_Problem("")
-        data_input = DataInput("ksrc 0 0 0")
-        data_input.link_to_problem(problem)
-        new_data = data_input.clone()
+
+def test_volume_merge():
+    vol = 1.0
+    list_node = syntax_node.ListNode("data")
+    list_node.append(syntax_node.ValueNode(str(vol), float))
+    node = syntax_node.SyntaxNode(
+        "volume",
+        {
+            "classifier": syntax_node.ValueNode("VoL", str),
+            "seperator": syntax_node.ValueNode("=", str),
+            "data": list_node,
+        },
+    )
+    card = volume.Volume(key="VoL", value=node, in_cell_block=True)
+    card2 = volume.Volume(key="VoL", value=node, in_cell_block=True)
+    with pytest.raises(MalformedInputError):
+        card.merge(card2)
+
+
+def test_volume_repr():
+    vol = 1.0
+    list_node = syntax_node.ListNode("data")
+    list_node.append(syntax_node.ValueNode(str(vol), float))
+    node = syntax_node.SyntaxNode(
+        "volume",
+        {
+            "classifier": syntax_node.ValueNode("VoL", str),
+            "seperator": syntax_node.ValueNode("=", str),
+            "data": list_node,
+        },
+    )
+    card = volume.Volume(key="VoL", value=node, in_cell_block=True)
+    assert "VOLUME" in repr(card)
+
+
+def test_data_clone():
+    problem = montepy.MCNP_Problem("")
+    data_input = DataInput("ksrc 0 0 0")
+    data_input.link_to_problem(problem)
+    new_data = data_input.clone()
 
 
 def test_classifier_start_comment():
