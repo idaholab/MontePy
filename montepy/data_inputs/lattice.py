fb795d321b8afd7ca9c1faf03466a78082f9c5cb
Travis J. Labossiere-Hickman <Travis.LabossiereHickman@INL.gov>
2025-10-09 09:41:51 -0600

    Alias HEXAHEDRAL and RECTANGULAR

--- montepy/data_inputs/lattice.py
+++ montepy/data_inputs/lattice.py
@@ -1,19 +1,20 @@
 # Copyright 2024 - 2025, Battelle Energy Alliance, LLC All Rights Reserved.
-from enum import Enum, unique
+from enum import Enum
 from warnings import warn
 
 
-@unique
 class LatticeType(Enum):
     """Represents the options for the lattice ``LAT``."""
 
-    HEXAHEDRAL = 1
+    RECTANGULAR = 1
+    """A rectangular prism is a type of hexahedron: solid with six faces."""
+    HEXAHEDRAL = RECTANGULAR
     """Hexhedra are solids with six faces.
-
+    
     One such solid is a rectangular prism.
     """
     HEXAGONAL = 2
-    """Hexagonal prism are solids with eight faces."""
+    """Hexagonal prisms are solids with eight faces."""
 
 
 class __DeprecatedLattice:
@@ -22,10 +23,10 @@ class __DeprecatedLattice:
     @property
     def HEXAHEDRA(self):
         warn(
-            message="Lattice.HEXAHEDRA is deprecated in favor of LatticeType.HEXAHEDRAL",
+            message="Lattice.HEXAHEDRA is deprecated in favor of LatticeType.RECTANGULAR",
             category=DeprecationWarning,
         )
-        return LatticeType.HEXAHEDRAL
+        return LatticeType.RECTANGULAR
 
     @property
     def HEXAGONAL(self):
@@ -36,4 +37,27 @@ class __DeprecatedLattice:
         return LatticeType.HEXAGONAL
 
 
+class __DeprecatedLatticeType:
+    """Helper for renamed attribute"""
+    def __call__(self, *args, **kwargs):
+        return LatticeType(*args, **kwargs)
+
+    @property
+    def RECTANGULAR(self):
+        return LatticeType.RECTANGULAR
+
+    @property
+    def HEXAGONAL(self):
+        return LatticeType.HEXAGONAL
+
+    @property
+    def HEXAHEDRAL(self):
+        warn(
+            message="LatticeType.HEXAHEDRAL is deprecated in favor of LatticeType.RECTANGULAR",
+            category=DeprecationWarning,
+        )
+        return LatticeType.RECTANGULAR
+
+
 Lattice = __DeprecatedLattice()
+# LatticeType = __DeprecatedLatticeType()
