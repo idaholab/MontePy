2a8749dfe33f344d392148de905490ea39927492
kordusas <bmarcinkevicius@gmail.com>
2025-10-12 19:58:46 +0200

    changed to isinstance. Comparing obj (last object read in BLOCK) to DataInputAbstract if so then we can trigger writting CELL data into DATA BLOCK if the flags are set

--- montepy/mcnp_problem.py
+++ montepy/mcnp_problem.py
@@ -14,6 +14,7 @@ from montepy.constants import DEFAULT_VERSION
 from montepy.materials import Material, Materials
 from montepy.surfaces import surface, surface_builder
 from montepy.surface_collection import Surfaces
+from montepy.data_inputs.data_input import DataInputAbstract
 
 # weird way to avoid circular imports
 from montepy.data_inputs import parse_data
@@ -613,8 +614,8 @@ class MCNP_Problem:
                     for line in lines:
                         inp.write(line + "\n")
 
-                # writing cell data in DATA BLOCK if needed otherwise adding termination line
-                if obj.__class__.__module__.startswith("montepy.data_inputs"):
+                # writing cell data in DATA BLOCK if the last written object inherits DataInputAbstract and there is cell data to write
+                if isinstance(obj, (DataInputAbstract)):
                     for line in self.cells._run_children_format_for_mcnp(
                         self.data_inputs, self.mcnp_version
                     ):
