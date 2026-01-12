24be846ee1c2c151123380ba45e3faee503406aa
Travis J. Labossiere-Hickman <Travis.LabossiereHickman@INL.gov>
2026-01-12 16:48:00 -0700

    Add SphereAtOrigin

--- montepy/surfaces/__init__.py
+++ montepy/surfaces/__init__.py
@@ -12,6 +12,7 @@ from .axis_plane import AxisPlane
 from .cylinder_par_axis import CylinderParAxis
 from .cylinder_on_axis import CylinderOnAxis
 from .sphere_on_axis import SphereOnAxis
+from .sphere_at_origin import SphereAtOrigin
 from .general_sphere import GeneralSphere
 from .general_plane import GeneralPlane
 from .half_space import HalfSpace, UnitHalfSpace
