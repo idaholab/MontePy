0a066b12a90f89967bdf344f3c38526da89da384
Micah D. Gale <micah.gale@inl.gov>
2026-03-05 11:30:43 -0600

    Lazily replicated bug #892 as a test case.

--- tests/test_importance.py
+++ tests/test_importance.py
@@ -2,6 +2,7 @@
 import montepy
 import os
 import io
+from pathlib import Path
 import pytest
 from montepy.cell import Cell
 from montepy.particle import Particle
@@ -317,3 +318,48 @@ class TestImportance:
         """Test that new cells have default importance of 1.0 (Issue #735)"""
         cell = montepy.Cell()
         assert cell.importance.neutron == 1.0
+
+    def test_print_explicit_default_imp(_):  # based on #892
+        problem = montepy.read_input(Path("tests") / "inputs" / "test.imcnp")
+
+        # Modify an existing cell. Importance remains.
+        problem.cells[1].geometry &= +problem.surfaces[1000]
+        # Cell 2 should give "imp:n=2.0 imp:p=2.0"
+        c2 = montepy.Cell(number=4)
+        c2.geometry = -problem.surfaces[1000] & +problem.surfaces[1005]
+        c2.importance.all = 2.0
+        problem.cells.append(c2)
+        # Cell 3 should give "imp:n=1.0 imp:p=1.0"
+        c3 = montepy.Cell(number=6)
+        c3.geometry = -problem.surfaces[1005] & +problem.surfaces[1010]
+        c3.importance.neutron = 1.0
+        c3.importance.photon = 1.0
+        problem.cells.append(c3)
+        # Cell 4 should give "imp:n=4.0 imp:p=1.0"
+        c4 = montepy.Cell(number=7)
+        c4.geometry = -problem.surfaces[1010]
+        c4.importance.neutron = 4.0
+        problem.cells.append(c4)
+        # Cell 5 DOES give "imp:n=5.0 imp:p=1.0"
+        c5 = montepy.Cell(number=8)
+        c5.geometry = -problem.surfaces[1015]
+        c5.importance.neutron = 5.0
+        c5.importance.photon = 1.0
+        problem.cells.append(c5)
+        # Ensure that this is set:
+        # Ensure that this is set:
+        problem.print_in_data_block["imp"] = False
+
+        stream = io.StringIO()
+        problem.write_problem(stream)
+        stream.seek(0)
+        new_problem = montepy.read_input(stream)
+        stream.close()
+        cells = new_problem.cells
+        for cell_num, imp_str in {
+            4: "imp:n=2.0 imp:p=2.0",
+            6: "imp:n=1.0 imp:p=1.0",
+            7: "imp:n=4.0 imp:p=1.0",
+            8: "imp:n=5.0 imp:p=1.0",
+        }.items():
+            assert imp_str in cells[cell_num]._input.input_text
