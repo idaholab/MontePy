6f8b12b6ee9d9e1f272c106e10c5526a976ce000
Micah Gale <mgale@montepy.org.>
2026-03-31 21:18:40 -0500

    Switched to star imports.

--- montepy/surfaces/__init__.py
+++ montepy/surfaces/__init__.py
@@ -3,57 +3,11 @@ from . import axis_plane
 from . import cylinder_par_axis
 from . import cylinder_on_axis
 from . import half_space
-from . import surface
 from . import surface_builder
 
 # promote objects
-from .surface import (
-    ArbitraryPolyhedron,
-    AxisAlignedQuadric,
-    AxisPlane,
-    Box,
-    ConeOnAxis,
-    ConeParAxis,
-    CylinderOnAxis,
-    CylinderParAxis,
-    Ellipsoid,
-    GeneralPlane,
-    GeneralQuadric,
-    GeneralSphere,
-    RectangularParallelepiped,
-    RightCircularCylinder,
-    RightEllipticalCylinder,
-    RightHexagonalPrism,
-    SphereMacrobody,
-    SphereAtOrigin,
-    SphereOnAxis,
-    Surface,
-    Torus,
-    TruncatedRightCone,
-    Wedge,
-    XCone,
-    XConeParAxis,
-    XCylinder,
-    XCylinderParAxis,
-    XPlane,
-    XSphere,
-    XTorus,
-    YCone,
-    YConeParAxis,
-    YCylinder,
-    YCylinderParAxis,
-    YPlane,
-    YSphere,
-    YTorus,
-    ZCone,
-    ZConeParAxis,
-    ZCylinder,
-    ZCylinderParAxis,
-    ZPlane,
-    ZSphere,
-    ZTorus,
-)
 from .half_space import HalfSpace, UnitHalfSpace
+from .surface import *
 from .surface_type import SurfaceType
 
 # promote functions
