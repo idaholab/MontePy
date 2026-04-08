87cd2d2ee493988c9c1860d57126b635bb5a696c
Micah Gale <mgale@montepy.org.>
2026-04-08 11:02:10 -0500

    Claude: wrote deprecations for all remaining classes.

--- montepy/surfaces/axis_plane.py
+++ montepy/surfaces/axis_plane.py
@@ -1,4 +1,44 @@
 # Copyright 2024-2026, Battelle Energy Alliance, LLC All Rights Reserved.
-from .surface import AxisPlane
+from .surface import AxisPlane as Parent
+import warnings
 
-__all__ = ["AxisPlane"]
+
+class AxisPlane(Parent):
+    """Represents surfaces PX, PY, PZ: a plane normal to a coordinate axis.
+
+    The surface equation (e.g. for PZ) is:
+
+    .. math::
+
+        z - d = 0
+
+    .. tip::
+
+        Since version 1.4.0 this has not been the preferred class for working with ``PX``, ``PY``, and ``PZ`` surfaces.
+        Instead :class:`~montepy.XPlane`, :class:`~montepy.YPlane`, and :class:`~montepy.ZPlane` are preferred.
+        There is no plan at this time to deprecate this class, but its use is not going to be promoted.
+
+    .. versionchanged:: 1.0.0
+
+        Added number parameter
+
+    .. deprecated:: 1.4.0
+
+       Access this class through ``montepy.AxisPlane`` instead.
+
+    Parameters
+    ----------
+    input : Union[Input, str]
+        The Input object representing the input
+    number : int
+        The number to set for this object.
+    surface_type : Union[SurfaceType, str]
+        The surface_type to set for this object
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "Submodule access to this class is deprecated. Use montepy.AxisPlane instead.",
+            category=DeprecationWarning,
+        )
+        super().__init__(*args, **kwargs)
--- montepy/surfaces/general_plane.py
+++ montepy/surfaces/general_plane.py
@@ -1,4 +1,38 @@
 # Copyright 2024-2026, Battelle Energy Alliance, LLC All Rights Reserved.
-from .surface import GeneralPlane
+from .surface import GeneralPlane as Parent
+import warnings
 
-__all__ = ["GeneralPlane"]
+
+class GeneralPlane(Parent):
+    """Represents surface P: a general plane.
+
+    The surface equation is:
+
+    .. math::
+
+        Ax + By + Cz - D = 0
+
+    May also be defined by three points (9 surface constants).
+
+    .. versionchanged:: 1.0.0
+
+        Added number parameter
+
+    .. deprecated:: 1.4.0
+
+       Access this class through ``montepy.GeneralPlane`` instead.
+
+    Parameters
+    ----------
+    input : Union[Input, str]
+        The Input object representing the input
+    number : int
+        The number to set for this object.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "Submodule access to this class is deprecated. Use montepy.GeneralPlane instead.",
+            category=DeprecationWarning,
+        )
+        super().__init__(*args, **kwargs)
