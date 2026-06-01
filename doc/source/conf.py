0f55b632cb9bc5e906332527542a18259923930b
Micah Gale <mgale@montepy.org>
2026-06-01 10:55:44 -0500

    Claude: hackily include the schema.org extension.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,3 +1,61 @@
+"""Sphinx extension to inject schema.org JSON-LD structured data.
+
+Add to conf.py:
+    extensions = [..., "schema_org"]
+
+    schema_org_configs = {
+        "index": {
+            "@type": "SoftwareApplication",
+            "name": "MontePy",
+            ...
+        }
+    }
+"""
+
+import json
+from typing import Any, Dict
+
+from docutils import nodes
+from sphinx.application import Sphinx
+
+
+def add_schema_org_data(app: Sphinx, pagename: str, templatename: str, context: Dict, doctree: Any) -> None:
+    """Add schema.org JSON-LD to page if configured."""
+    configs = app.config.schema_org_configs
+    if not configs or pagename not in configs:
+        return
+
+    schema_data = configs[pagename]
+    schema_json = json.dumps(schema_data, indent=2)
+    context['schema_org_data'] = schema_json
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+    """Register the extension."""
+    app.add_config_value("schema_org_configs", {}, "html")
+    app.connect("html-page-context", add_schema_org_data)
+
+    return {
+        "version": "0.1.0",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
+(base)
+(mgale@INL436227)-(~/dev/montepy) (seo_opt)
+ ^_^->cat doc/source/_templates/page.html
+{% extends "!page.html" %}
+
+{% block body %}
+  {{ super() }}
+  {% if pagename == 'index' and schema_org_data %}
+  <script type="application/ld+json">
+  {{ schema_org_data | safe }}
+  </script>
+  {% endif %}
+{% endblock %}
+(base)
+(mgale@INL436227)-(~/dev/montepy) (seo_opt)
+ ^_^->cat doc/source/conf.py
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
